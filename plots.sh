9dba24daf8799a76e6c79c88ae02a2a4f0b80e17
Jindrich Lidrych <jindrich.lidrych@cern.ch>
2022-03-15 13:01:17 +0100

    Add plots.sh

---
+++ plots.sh
@@ -0,0 +1,7 @@
+#python HHplotter.py --sample_dir /eos/cms/store/group/phys_higgs/HiggsExo/HH_bbZZ_bbllqq/jlidrych/v5/2016/ --hist_dir ../../HHCoffea/histo_dir/InputV16/Electron/2016 --xfile ../../HHCoffea/xsection/xsections_2016.yaml --year 2016 --outdir plots_2016_v16-v1 --channel electron
+
+#python HHplotter.py --sample_dir /eos/cms/store/group/phys_higgs/HiggsExo/HH_bbZZ_bbllqq/jlidrych/v5/2017/ --hist_dir ../../HHCoffea/histo_dir/InputV16/Electron/2017 --xfile ../../HHCoffea/xsection/xsections_2017.yaml --year 2017 --outdir plots_2017_v16-v1 --channel electron
+
+#python HHplotter.py --sample_dir /eos/cms/store/group/phys_higgs/HiggsExo/HH_bbZZ_bbllqq/jlidrych/v5/2018/ --hist_dir ../../HHCoffea/histo_dir/InputV16/Electron/2018 --xfile ../../HHCoffea/xsection/xsections_2018.yaml --year 2018 --outdir plots_2018_v16-v1 --channel electron
+
+python HHplotter.py --sample_dir /eos/cms/store/group/phys_higgs/HiggsExo/HH_bbZZ_bbllqq/jlidrych/v5/2017/ --hist_dir ../../HHCoffea/histo_dir/InputV17v1/Electron/2017 --xfile ../../HHCoffea/xsection/xsections_2017.yaml --year 2017 --outdir plots_2017_v16-v1-test --channel electron
