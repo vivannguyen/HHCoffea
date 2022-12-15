d5bd1316800c3374d57e98a7d3a41f120f4891c2
vivannguyen <vivantnguyen@gmail.com>
2022-12-15 18:25:39 +0100

    condor submission for coffea on unmerged files

--- run_HHProducer.py
+++ run_HHProducer.py
@@ -12,7 +12,6 @@ logging.basicConfig(level=logging.DEBUG)
 script_TEMPLATE = """#!/bin/bash
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-source /cvmfs/unpacked.cern.ch/registry.hub.docker.com/coffeateam/coffea-dask:latest
 export SCRAM_ARCH=slc6_amd64_gcc630
 
 cd {cmssw_base}/src/
@@ -29,11 +28,17 @@ echo "+ CMSSW_BASE  = $CMSSW_BASE"
 echo "+ PYTHON_PATH = $PYTHON_PATH"
 echo "+ PWD         = $PWD"
 cp $2 temp_$1.root
-python condor_HH_WS.py --jobNum=$1 --isMC={ismc} --era={era} --infile=$2
+
+INFILE=$2
+echo "INFILE $INFILE"
+python condor_HH_WS.py --jobNum=$1 --isMC={ismc} --era={era} --infile=$INFILE
 
 echo "----- transfer output to eos :"
-
-xrdcp -s -f tree_{era}_WS.root {eosoutdir}
+NUM_EXT="${{INFILE##*_}}"
+echo $NUM_EXT
+NUM="${{NUM_EXT%.root}}"
+echo "LOOK AT ME $NUM"
+xrdcp -s -f tree_{era}_WS.root {eosoutdir}/{sample_dir}_tree_${{NUM}}_WS.root
 
 echo "----- directory after running :"
 
@@ -44,7 +49,6 @@ echo " ------ THE END (everyone dies !) ----- "
 
 
 condor_TEMPLATE = """
-request_disk          = 1024
 executable            = {jobdir}/script.sh
 arguments             = $(ProcId) $(jobid)
 transfer_input_files  = {transfer_file}
@@ -59,6 +63,7 @@ Requirements = HasSingularity
 queue jobid from {jobdir}/inputfiles.dat
 """
 
+#xrdcp -s -f {sample_dir}_tree_${{NUM}}_WS.root {eosoutdir}
 def main():
     parser = argparse.ArgumentParser(description='Famous Submitter')
     parser.add_argument("-t"   , "--tag"   , type=str, default="Exorcism"  , help="production tag", required=True)
@@ -72,22 +77,23 @@ def main():
     options = parser.parse_args()
 
     cmssw_base = os.environ['CMSSW_BASE']
-    indir = "/eos/cms/store/group/phys_higgs/HiggsExo/HH_bbZZ_bbllqq/test/condortesting/"
+    indir = "/eos/cms/store/group/phys_higgs/HiggsExo/HH_bbZZ_bbllqq/test/DYtest/"
     eosbase = "/eos/cms/store/group/phys_higgs/HiggsExo/HH_bbZZ_bbllqq/vivan/{tag}/{year}/{sample}/"
 
-    for sample in os.listdir(indir):
+    def do_the_thing(sample, sample_dir=''):
+        sample_joined = os.path.join(sample_dir, sample.replace('.root', ''))
         if "merged" in sample:
-            continue
+            return
         if "WS" in sample:
-            continue
+            return
         sample = sample.replace('.root', '')
-        jobs_dir = '_'.join(['jobs', options.tag, sample])
-        logging.info("-- sample_name : " + sample)
+        jobs_dir = '_'.join(['jobs', options.tag,'_'.join([sample_dir,sample])])
+        logging.info("-- sample_name : " + sample_joined)
 
         if os.path.isdir(jobs_dir):
             if not options.force:
                 logging.error(" " + jobs_dir + " already exist !")
-                continue
+                return
             else:
                 logging.warning(" " + jobs_dir + " already exists, forcing its deletion!")
                 shutil.rmtree(jobs_dir)
@@ -97,12 +103,15 @@ def main():
             os.mkdir(jobs_dir)
 
         with open(os.path.join(jobs_dir, 'inputfiles.dat'), 'w') as infiles:
-            infiles.write(os.path.join(indir, "{sample}.root".format(sample=sample)))
+            infiles.write(os.path.join(indir, "{sample}.root".format(sample=sample_joined)))
             infiles.close()
 
         time.sleep(1)
+        if sample_dir == '':
+            eosoutdir = eosbase.format(tag=options.tag + "_ws", year=options.era, sample=sample)
+        else:
+            eosoutdir = eosbase.format(tag=options.tag + "_ws", year=options.era, sample=sample_dir)
 
-        eosoutdir = eosbase.format(tag=options.tag + "_ws", year=options.era, sample=sample)
         if '/eos/cms' in eosoutdir:
             eosoutdir = eosoutdir.replace('/eos/cms', 'root://eoscms.cern.ch/')
             os.system("eos mkdir -p {}".format(eosoutdir.replace('root://eoscms.cern.ch/','')))
@@ -115,6 +124,7 @@ def main():
                 ismc=options.isMC,
                 era=options.era,
                 eosoutdir=eosoutdir,
+                sample_dir=sample_dir,
             )
             scriptfile.write(script)
             scriptfile.close()
@@ -126,7 +136,9 @@ def main():
                     "../python/SumWeights.py",
                     "../python/HH_Producer.py",
                     "../btag_weights.jsonl",
-                    "../2016newfinalselecttest"
+                    "../2016newfinalselecttest",
+                    "../2017newfinalselecttest",
+                    "../2018newfinalselecttest"
                 ]),
                 jobdir=jobs_dir,
                 queue=options.queue
@@ -135,7 +147,7 @@ def main():
             condorfile.close()
 
         if options.dryrun:
-            continue
+            return
         
         htc = subprocess.Popen(
             "condor_submit " + os.path.join(jobs_dir, "condor.sub"),
@@ -148,6 +160,88 @@ def main():
         out, err = htc.communicate()
         exit_status = htc.returncode
         logging.info("condor submission status : {}".format(exit_status))
-        
+
+
+    for sample in os.listdir(indir):
+        if os.path.isfile(os.path.join(indir, sample)):
+            do_the_thing(sample)
+        elif os.path.isdir(os.path.join(indir, sample)):
+            for fi in os.listdir(os.path.join(indir, sample)):
+                do_the_thing(fi, sample)
+
+#    for sample in os.listdir(indir):
+#        if "merged" in sample:
+#            continue
+#        if "WS" in sample:
+#            continue
+#        sample = sample.replace('.root', '')
+#        jobs_dir = '_'.join(['jobs', options.tag, sample])
+#        logging.info("-- sample_name : " + sample)
+#
+#        if os.path.isdir(jobs_dir):
+#            if not options.force:
+#                logging.error(" " + jobs_dir + " already exist !")
+#                continue
+#            else:
+#                logging.warning(" " + jobs_dir + " already exists, forcing its deletion!")
+#                shutil.rmtree(jobs_dir)
+#                #os.system("rm -rf {}".format(jobs_dir))
+#                os.mkdir(jobs_dir)
+#        else:
+#            os.mkdir(jobs_dir)
+#
+#        with open(os.path.join(jobs_dir, 'inputfiles.dat'), 'w') as infiles:
+#            infiles.write(os.path.join(indir, "{sample}.root".format(sample=sample)))
+#            infiles.close()
+#
+#        time.sleep(1)
+#
+#        eosoutdir = eosbase.format(tag=options.tag + "_ws", year=options.era, sample=sample)
+#        if '/eos/cms' in eosoutdir:
+#            eosoutdir = eosoutdir.replace('/eos/cms', 'root://eoscms.cern.ch/')
+#            os.system("eos mkdir -p {}".format(eosoutdir.replace('root://eoscms.cern.ch/','')))
+#        else:
+#            raise NameError(eosoutdir)
+#
+#        with open(os.path.join(jobs_dir, "script.sh"), "w") as scriptfile:
+#            script = script_TEMPLATE.format(
+#                cmssw_base=cmssw_base,
+#                ismc=options.isMC,
+#                era=options.era,
+#                eosoutdir=eosoutdir,
+#            )
+#            scriptfile.write(script)
+#            scriptfile.close()
+#
+#        with open(os.path.join(jobs_dir, "condor.sub"), "w") as condorfile:
+#            condor = condor_TEMPLATE.format(
+#                transfer_file= ",".join([
+#                    "../condor_HH_WS.py",
+#                    "../python/SumWeights.py",
+#                    "../python/HH_Producer.py",
+#                    "../btag_weights.jsonl",
+#                    "../2016newfinalselecttest"
+#                ]),
+#                jobdir=jobs_dir,
+#                queue=options.queue
+#            )
+#            condorfile.write(condor)
+#            condorfile.close()
+#
+#        if options.dryrun:
+#            continue
+#        
+#        htc = subprocess.Popen(
+#            "condor_submit " + os.path.join(jobs_dir, "condor.sub"),
+#            shell  = True,
+#            stdin  = subprocess.PIPE,
+#            stdout = subprocess.PIPE,
+#            stderr = subprocess.PIPE,
+#            close_fds=True
+#        )
+#        out, err = htc.communicate()
+#        exit_status = htc.returncode
+#        logging.info("condor submission status : {}".format(exit_status))
+#        
 if __name__ == "__main__":
     main()
